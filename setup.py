2bd373549930583b42364d36c66e449a0a04c9e4
Martin Ueding <dev@martin-ueding.de>
2014-05-08 21:36:39 +0200

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+
+from setuptools import setup, find_packages
+
+setup(
+    author="Martin Ueding",
+    author_email="dev@martin-ueding.de",
+    description="Plots some data files",
+    license="GPL2",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python",
+    ],
+    name="fastplot",
+    packages=find_packages(),
+    scripts=[
+        'fastplot',
+    ],
+    install_requires=[
+        'matplotlib',
+        'numpy',
+    ],
+    url="https://github.com/martin-ueding/fastplot",
+    download_url="http://martin-ueding.de/download/fastplot/",
+    version="1.0",
+)
